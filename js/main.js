0cf5249dd8d3a76117f81c51ddfbfe61abb81284
Francesco Sangalli <francesco.sangalli89@gmail.com>
2022-07-05 16:44:25 +0200

    inizialization: biglietto del treno form

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+let kmDaPercorrereDom = parseInt(document.getElementById('kmDaPercorrere'));
+console.log(typeof kmDaPercorrereDom);
+console.log(kmDaPercorrereDom);
+
+const costoXKm = 0.21;
+console.log(typeof costoXKm);
+console.log(costoXKm);
+
+let costoTotaleXKm = kmDaPercorrereDom * costoXKm;
+console.log(typeof costoTotaleXKm);
+console.log(costoTotaleXKm);
+
+const generaDom = document.getElementById('genera');
+
