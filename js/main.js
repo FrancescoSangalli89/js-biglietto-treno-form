9170ea7d51d0bc04ee9a99697831edf78e811633
Francesco Sangalli <francesco.sangalli89@gmail.com>
2022-07-06 01:03:15 +0200

    corrected code

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ const generaDom = document.getElementById('genera');
 
 generaDom.addEventListener('click', 
     function() {
+        let nomePasseggeroDom = document.getElementById('nome').value;
+
+        document.querySelector('.nomePasseggero').innerHTML = nomePasseggeroDom;
+
         const kmDaPercorrere = parseInt(document.getElementById('km').value);
         console.log(typeof kmDaPercorrere);
 
@@ -13,26 +17,21 @@ generaDom.addEventListener('click',
 
         let fasciaPrezzo = parseInt(document.getElementById('eta').value);
 
-        let nomePasseggeroDom = document.getElementById('nome').value;
-
-        document.querySelector('.nomePasseggero').innerHTML = '<h3>' + nomePasseggeroDom + '</h3>';
-
         if (fasciaPrezzo == 1) {
-            document.querySelector('.offer').innerHTML = '<div>Biglietto standard</div>';
-            document.querySelector('.ticketPrice').innerHTML = '<div>' + Math.round((costoTotaleXKm) * 100) / 100 + '€</div>';
+            document.querySelector('.offer').innerHTML = 'Biglietto standard';
+            document.querySelector('.ticketPrice').innerHTML = Math.round((costoTotaleXKm) * 100) / 100 + '€';
         } else if (fasciaPrezzo == 2) {
-            document.querySelector('.offer').innerHTML = '<div>Sconto 20%</div>';
-            document.querySelector('.ticketPrice').innerHTML = '<div>' + Math.round((costoTotaleXKm - costoTotaleXKm * 20 / 100) * 100) / 100 + '€</div>';
+            document.querySelector('.offer').innerHTML = 'Sconto 20%';
+            document.querySelector('.ticketPrice').innerHTML = Math.round((costoTotaleXKm - costoTotaleXKm * 20 / 100) * 100) / 100 + '€';
         } else if (fasciaPrezzo == 3) {
-            document.querySelector('.offer').innerHTML = '<div>Sconto 40%</div>';
-            document.querySelector('.ticketPrice').innerHTML = '<div>' + Math.round((costoTotaleXKm - costoTotaleXKm * 40 / 100) * 100) / 100 + '€</div>';
+            document.querySelector('.offer').innerHTML = 'Sconto 40%';
+            document.querySelector('.ticketPrice').innerHTML = Math.round((costoTotaleXKm - costoTotaleXKm * 40 / 100) * 100) / 100 + '€';
         }
 
-        document.querySelector('.carrozza').innerHTML = '<div>' + (Math.floor(Math.random() * 21) + 1) + '</div>';
+        document.querySelector('.carrozza').innerHTML = Math.floor(Math.random() * 21) + 1;
 
-        document.querySelector('.codeCp').innerHTML = '<div>' + (Math.floor(Math.random() * 10000) + 90000) + '</div>';
+        document.querySelector('.codeCp').innerHTML = Math.floor(Math.random() * 10000) + 90000;
         
-
         document.querySelector('.ticket-container').classList.add('show');
     }
 )
